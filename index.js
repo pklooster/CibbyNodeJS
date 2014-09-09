587b635dcfff9b4e7ea4b97c8148fc1bab127c0c
Jorijn Schrijvershof <jorijn@jorijn.com>
2014-09-09 11:07:26 +0200

    kind of alpha version

--- index.js
+++ index.js
@@ -1,34 +1,77 @@
-var irc = require('slate-irc');
-var net = require('net');
-var db = require('mongodb');
-var cfg = require('./config');
+var irc = require('slate-irc'),
+    net = require('net'),
+    mongo = require('mongodb'),
+    monk = require('monk'),
+    cfg = require('./config')
 
-var activeConfig = cfg.getConfig();
+var ac = cfg.getConfig();
+var db = monk(ac.mongodb.connstr);
 
 var stream = net.connect({
-  port: activeConfig.server.port,
-  host: activeConfig.server.host
+  port: ac.server.port,
+  host: ac.server.host
 });
 
 var client = irc(stream);
 
-client.nick(activeConfig.who.nickname);
-client.user(activeConfig.who.username, activeConfig.who.realname);
+client.nick(ac.who.nickname);
+client.user(ac.who.username, ac.who.realname);
 
 function cibby(irc) {
-    irc.on('welcome', function() {
-        client.join('#home');
-        client.names('#home', function(err, names){
-          console.log(names);
+    var activity = {};
+
+    function runAI(channel) {
+        var offset = (24 * 60 * 60 * 1000) * ac.ai.dateThreshold,
+            dateObj = new Date(),
+            query;
+
+        dateObj.setTime(dateObj.getTime() - offset);
+        query = { datetime: { $gt: dateObj }, to: channel };
+
+        // get a random message matching 'query'
+        var dbt = db.get('messages'), rand;
+
+        dbt.count(query, function(err, count) {
+            rand = Math.floor(Math.random() * count);
+            dbt.find(query, { limit: 1, skip: rand }, function(err, res) {
+                if (typeof res[0] === 'object') {
+                    // SPLURT
+                    client.send(channel, res[0].message);
+                }
+
+                // reset.
+                activity[channel] = null;
+            });
         });
+    }
+
+    irc.on('welcome', function() {
+        for (var i = ac.server.join.length - 1; i >= 0; i--) {
+            client.join(ac.server.join[i]);
+        }
     });
 
     irc.on('message', function(message) {
+        // only for public channels
         if (message.to.substr(0, 1) !== '#') {
             return;
         }
 
-        client.send(message.to, message.from + ' zegt: ' + message.message);
+        // insert this crap into our database
+        var dbt = db.get('messages');
+        dbt.insert({
+            who: message.from,
+            to: message.to,
+            message: message.message,
+            datetime: new Date()
+        });
+
+        // set flag to run somewhere between 1 and 180 seconds after this message
+        if (message.to in activity === false || activity[message.to] === null) {
+            var runAfter = (Math.floor((Math.random() * 180) + 1)) * 1000;
+            console.log('running runAI() after ' + (runAfter / 1000) + ' seconds.');
+            activity[message.to] = setTimeout(function() { runAI(message.to); }, runAfter);
+        }
     })
 }
 
